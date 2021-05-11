80322862f09122fd663deae1f20c3eafcb449369
tbash4015 <tombashford4015@gmail.com>
2021-05-11 03:36:45 +0100

    Removed prototype example Commands, not relevant to final solution - Time, Ask, Books, Message

--- demo.php
+++ demo.php
@@ -5,18 +5,10 @@
 require __DIR__.'/vendor/autoload.php';
 
 use Backend\Command\SalaryCommand;
-use Backend\Command\BooksCommand;
-use Backend\Command\MessageCommand;
-use Backend\Command\AskNameCommand;
-use Backend\Command\TimeCommand;
 use Symfony\Component\Console\Application;
 
 $app = new Application();
 
 $app->add(new SalaryCommand());
-$app->add(new BooksCommand());
-$app->add(new MessageCommand());
-$app->add(new AskNameCommand());
-$app->add(new TimeCommand());
 
 $app->run();
