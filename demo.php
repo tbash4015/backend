e37a0e97d42da67bfafadf3785bfd1d2f55826fc
tbash4015 <tombashford4015@gmail.com>
2021-05-11 10:34:52 +0100

    Title and version number given to CLI application

--- demo.php
+++ demo.php
@@ -7,7 +7,7 @@ require __DIR__.'/vendor/autoload.php';
 use Backend\Command\SalaryCommand;
 use Symfony\Component\Console\Application;
 
-$app = new Application();
+$app = new Application("Salary Dates Generator", "- version 1.0");
 
 $app->add(new SalaryCommand());
 
